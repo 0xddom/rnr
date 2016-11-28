67c6751fa0f31c6524d3b4c6361363fc6cd3e003
Daniel Domínguez <danieldominguez05@gmail.com>
2016-11-28 20:56:12 +0100

    finish looteable

--- RnR/Systems/D20/FloorElements/FoodGrass.cs
+++ RnR/Systems/D20/FloorElements/FoodGrass.cs
@@ -1,18 +1,21 @@
-﻿using System;
-using RnR.Systems.D20.Base.Actors;
+﻿using RnR.Systems.D20.Base.Actors;
 using RnR.Systems.D20.Base.FloorElements;
+using RnR.Systems.D20.Base.Objects;
 
-// XXX: Pending for objects implementation
 namespace RnR.Systems.D20.FloorElements
 {
 	public class FoodGrass : AbstractGrass
 	{
-		public FoodGrass ()
+		private AbstractFood food;
+
+		public FoodGrass (AbstractFood food)
 		{
+			this.food = food;
 		}
 
 		protected override void ApplyAction (GameActor target)
 		{
+			target.Inventory.Add (food);
 		}
 	}
 }
